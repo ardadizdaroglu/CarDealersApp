153024c70db46260493b256671696bb44e393d5f
ardadizdarogluacc <arda.dizdaroglu@accenture.com>
2023-03-12 00:14:48 +0300

    implementing usecase

---
+++ app/src/main/java/com/adizdaroglu/cardealersapp/domain/usecase/GetCarsUseCase.kt
@@ -0,0 +1,27 @@
+package com.adizdaroglu.cardealersapp.domain.usecase
+
+import com.adizdaroglu.cardealersapp.common.Resource
+import com.adizdaroglu.cardealersapp.data.remote.dto.toCar
+import com.adizdaroglu.cardealersapp.domain.model.Car
+import com.adizdaroglu.cardealersapp.domain.repository.CarRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCarsUseCase @Inject constructor(
+    private val repository: CarRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Car>>> = flow {
+        try {
+            emit(Resource.Loading<List<Car>>())
+            val cars = repository.getCars().map { it.toCar() }
+            emit(Resource.Success<List<Car>>(cars))
+        } catch(e: HttpException) {
+            emit(Resource.Error<List<Car>>(e.localizedMessage ?: "An unexpected error occured"))
+        } catch(e: IOException){
+            emit(Resource.Error<List<Car>>("Couldn't reach to server. Check your internet connection."))
+        }
+    }
+}
